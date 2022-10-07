5a463727ffbb1108256a11fbcbec528cdb3ae352
tomit4 <mosssap@gmail.com>
2022-10-07 15:56:31 -0700

    :construction: Playing with navbar animation

--- remaster/script.js
+++ remaster/script.js
@@ -1,14 +1,23 @@
 const navi = document.querySelector('.navi')
 const hamburger = document.querySelector('.hamburger')
 const dots = document.querySelector('#dots')
+const icons = document.querySelector(".icons")
 
-navi.addEventListener('click', (e) => {
-    navi.classList.remove('hamburger')
-    navi.classList.add('navbar')
-})
+
+navi.addEventListener('click', expand)
+
+function expand() {
+    navi.classList.add('expanded')
+    setTimeout(() => {
+        navi.classList.add('navbar')
+        navi.classList.remove('expanded', 'hamburger')
+        navi.removeEventListener('click', expand, false)
+        icons.style.visibility = "visible"
+    }, 1250)
+}
 
 dots.addEventListener('click', (e) => {
     e.stopPropagation()
-    navi.classList.remove('navbar')
+    navi.classList.remove('navbar', 'expanded')
     navi.classList.add('hamburger')
 })
