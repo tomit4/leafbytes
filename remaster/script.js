5868576c12117b0c57370cd04e3e8d273ee045be
tomit4 <mosssap@gmail.com>
2022-10-07 21:02:56 -0700

    :lipstick: Added responsive animated navbar

--- remaster/script.js
+++ remaster/script.js
@@ -1,7 +1,8 @@
 const navi = document.querySelector('.navi')
 const hamburger = document.querySelector('.hamburger')
-const dots = document.querySelector('#dots')
-const icons = document.querySelector(".icons")
+// const dots = document.querySelector('.dots')
+const icons = document.getElementsByClassName("icons")
+const dots = document.getElementsByClassName("dots")
 
 
 navi.addEventListener('click', expand)
@@ -12,12 +13,31 @@ function expand() {
         navi.classList.add('navbar')
         navi.classList.remove('expanded', 'hamburger')
         navi.removeEventListener('click', expand, false)
-        icons.style.visibility = "visible"
+        for (let i = 0; i < icons.length; i++) {
+            icons[i].style.visibility = "visible"
+        }
+        for (let j = 0; j < dots.length; j++) {
+            dots[j].style.visibility = "visible"
+            addDotEvents(j)
+        }
     }, 1250)
 }
 
-dots.addEventListener('click', (e) => {
-    e.stopPropagation()
-    navi.classList.remove('navbar', 'expanded')
-    navi.classList.add('hamburger')
-})
+function addDotEvents(j) {
+    dots[j].addEventListener('click', (e) => {
+        e.stopPropagation()
+        navi.addEventListener('click', expand)
+        for (let i = 0; i < icons.length; i++) {
+                icons[i].style.visibility = "hidden"
+        }
+        for (let j = 0; j < dots.length; j++) {
+            dots[j].style.visibility = "hidden"
+        }
+        navi.classList.add('retracted')
+        setTimeout(() => {
+            navi.classList.remove('navbar')
+            navi.classList.add('hamburger')
+            navi.classList.remove('retracted')
+        }, 1250)
+    })
+}
