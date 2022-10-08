fb4e978e23be79086b3f1edefb53ddbe2204b264
tomit4 <mosssap@gmail.com>
2022-10-08 00:27:15 -0700

    :lipstick: Added fade in and scaling of icons

--- remaster/script.js
+++ remaster/script.js
@@ -1,10 +1,17 @@
 const navi = document.querySelector('.navi')
 const hamburger = document.querySelector('.hamburger')
-// const dots = document.querySelector('.dots')
-const icons = document.getElementsByClassName("icons")
-const dots = document.getElementsByClassName("dots")
+const icons = document.getElementsByClassName('icons')
+const dots = document.getElementsByClassName('dots')
 
+/* icon scale up */
+function scaleUp(e) {
+    const selectedIcon = document.getElementById(e.id)
+    selectedIcon.classList.toggle('scaled')
+    /* check if other icons have been hit, and if so,
+     * remove scaled class from all icons except for one recently clicked */
+}
 
+/* navbar expansion/contraction */
 navi.addEventListener('click', expand)
 
 function expand() {
@@ -13,26 +20,34 @@ function expand() {
         navi.classList.add('navbar')
         navi.classList.remove('expanded', 'hamburger')
         navi.removeEventListener('click', expand, false)
+
         for (let i = 0; i < icons.length; i++) {
-            icons[i].style.visibility = "visible"
+            icons[i].style.visibility = 'visible'
+            icons[i].classList.add('fade-in')
         }
+
         for (let j = 0; j < dots.length; j++) {
-            dots[j].style.visibility = "visible"
+            dots[j].style.visibility = 'visible'
+            dots[j].classList.add('fade-in')
             addDotEvents(j)
         }
     }, 1250)
 }
 
 function addDotEvents(j) {
-    dots[j].addEventListener('click', (e) => {
+    dots[j].addEventListener('click', e => {
         e.stopPropagation()
         navi.addEventListener('click', expand)
+
         for (let i = 0; i < icons.length; i++) {
-                icons[i].style.visibility = "hidden"
+            icons[i].style.visibility = 'hidden'
+            icons[i].classList.remove('fade-in', 'scaled')
         }
         for (let j = 0; j < dots.length; j++) {
-            dots[j].style.visibility = "hidden"
+            dots[j].style.visibility = 'hidden'
+            dots[j].classList.remove('fade-in', 'scaled')
         }
+
         navi.classList.add('retracted')
         setTimeout(() => {
             navi.classList.remove('navbar')
