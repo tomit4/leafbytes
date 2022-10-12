2e90e2896969825dc200a500ea3e1cb7f7d853ee
tomit4 <mosssap@gmail.com>
2022-10-11 20:46:41 -0700

    :construction: Added animation to portfolio tree root

--- remaster/script-articles.js
+++ remaster/script-articles.js
@@ -1,16 +1,25 @@
 /* article-specific js */
 let treeToggled = false
 function showTree(className) {
-    const cName = document.getElementsByClassName(`${className}-tree`)
+    const treeName = document.getElementsByClassName(`${className}-tree`)
+    const vertTreeName = document.getElementsByClassName(`${className}-tree-vert`)
+
+
     if (treeToggled === false) {
-        for (let i = 0; i < cName.length; i++) {
-            cName[i].classList.remove(`${className}-tree-hidden`)
-            cName[i].classList.add(`${className}-tree-show`)
+        for (let i = 0; i < treeName.length; i++) {
+            treeName[i].classList.remove(`${className}-tree-hidden`)
+            treeName[i].classList.add(`${className}-tree-show`)
+        }
+        for (let i = 0; i < vertTreeName.length; i++) {
+            vertTreeName[i].classList.add(`${className}-tree-vert-show`)
         }
     } else {
-        for (let i = 0; i < cName.length; i++) {
-            cName[i].classList.add(`${className}-tree-hidden`)
-            cName[i].classList.remove(`${className}-tree-show`)
+        for (let i = 0; i < treeName.length; i++) {
+            treeName[i].classList.add(`${className}-tree-hidden`)
+            treeName[i].classList.remove(`${className}-tree-show`)
+        }
+        for (let i = 0; i < vertTreeName.length; i++) {
+            vertTreeName[i].classList.remove(`${className}-tree-vert-show`)
         }
     }
     treeToggled = !treeToggled
