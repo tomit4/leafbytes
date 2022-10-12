a39b94444d98e870f2232e9738d6ddb88ec6e69d
tomit4 <mosssap@gmail.com>
2022-10-11 19:56:29 -0700

    :construction: Continued with subject trees

--- remaster/script-articles.js
+++ remaster/script-articles.js
@@ -1,8 +1,17 @@
 /* article-specific js */
-
-function isLinkItem(className) {
-    const name = document.querySelector(`${className}`)
-    name.addEventListener('click', () => {
-        console.log(`${className} was clicked`)
-    })
+let treeToggled = false
+function showTree(className) {
+    const cName = document.getElementsByClassName(`${className}-tree`)
+    if (treeToggled === false) {
+        for (let i = 0; i < cName.length; i++) {
+            cName[i].classList.remove(`${className}-tree-hidden`)
+            cName[i].classList.add(`${className}-tree-show`)
+        }
+    } else {
+        for (let i = 0; i < cName.length; i++) {
+            cName[i].classList.add(`${className}-tree-hidden`)
+            cName[i].classList.remove(`${className}-tree-show`)
+        }
+    }
+    treeToggled = !treeToggled
 }
