18025c477f7340482596ea493e5c44353ba0b712
tomit4 <mosssap@gmail.com>
2022-11-15 19:40:47 -0800

    :sparkles: Finished up keyboard-centric workflow article

--- script.js
+++ script.js
@@ -371,13 +371,16 @@ async function renderIt(articleId) {
         .then((async (html) => {
             if (isAtDesktopDimensions) {
                 cachedArticle = html
-                for (let i = 0; i < articleDesktop.length; i++)
+                for (let i = 0; i < articleDesktop.length; i++) {
                     articleDesktop[i].innerHTML = html
+                    articleDesktop[i].scrollTop = 0
+                }
             }
             else {
                 await wait(1000)
                 cachedArticle = html
                 article.innerHTML = html
+                article.scrollTop = 0
                 for (let i = 0; i < articleDesktop.length; i++)
                     body.removeChild(articleDesktop[i])
             }
